9fd0430ffabb16feed8b5e9713d893ea42d66e5a
HijiriSato88 <satohiji0808@outlook.com>
2024-12-30 15:40:20 +0900

    LINEのレスポンスメッセージの冒頭に都市名も追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,8 @@ class User < ApplicationRecord
             response = HTTParty.get("http://api.openweathermap.org/data/2.5/forecast?q=#{location},jp&APPID=#{API_KEY}&lang=ja&units=metric")
             if response.success?
                 data = response.parsed_response
-                data['list'][1..6].map do |item|
+                city_name = data['city']['name']
+                forecasts = data['list'][1..6].map do |item|
                     {
                         temp_max: (item['main']['temp_max']).round(2),
                         temp_min: (item['main']['temp_min']).round(2),
@@ -16,22 +17,29 @@ class User < ApplicationRecord
                         dt_txt: item['dt_txt'],
                     }
                 end
+                { city_name: city_name, forecasts: forecasts }
             else
                 Rails.logger.error("Weather API request failed: #{response.code} #{response.message}")
                 nil
             end
         end
 
-        def format_weather_responses(weather_forecasts)
-            # 複数の天気データをフォーマット
-            weather_forecasts.map do |forecast|
+        def format_weather_responses(weather_data)
+            city_name = weather_data[:city_name]
+            forecasts = weather_data[:forecasts]
+
+            message = "都市: #{city_name}\n\n"
+
+            message += forecasts.map do |forecast|
                 <<~TEXT
                     日時: #{forecast[:dt_txt]}
                     天気: #{forecast[:description]}
                     気温: #{forecast[:temp_min]}°C ~ #{forecast[:temp_max]}°C
                     湿度: #{forecast[:humidity]}%
                 TEXT
-            end.join("\n\n") # 各テキストを区切る改行を追加
+            end.join("\n\n") #各テキストを区切る改行
+
+            message
         end
     end
 end
