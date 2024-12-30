71905a7e84d1d122f8563a9fec45efe495ba8b54
HijiriSato88 <satohiji0808@outlook.com>
2024-12-30 15:30:23 +0900

    予報を含めた６件の3時間ごとのデータを取得し、LINEに出力

--- app/controllers/line_bot_controller.rb
+++ app/controllers/line_bot_controller.rb
@@ -1,44 +1,43 @@
 class LineBotController < ApplicationController
-	def client
-		@client ||= Line::Bot::Client.new { |config|
-			config.channel_secret = ENV['LINE_CHANNEL_SECRET']
-			config.channel_token = ENV['LINE_CHANNEL_TOKEN']
-		}
-	end
+    def client
+        @client ||= Line::Bot::Client.new { |config|
+            config.channel_secret = ENV['LINE_CHANNEL_SECRET']
+            config.channel_token = ENV['LINE_CHANNEL_TOKEN']
+        }
+    end
 
-	def callback
-		body = request.body.read
+    def callback
+        body = request.body.read
 
-		signature = request.env['HTTP_X_LINE_SIGNATURE']
-		unless client.validate_signature(body, signature)
-			head 470
-		end
+        signature = request.env['HTTP_X_LINE_SIGNATURE']
+        unless client.validate_signature(body, signature)
+            head 470
+        end
 
-		events = client.parse_events_from(body)
+        events = client.parse_events_from(body)
 
-		events.each do |event|
-			case event
-			when Line::Bot::Event::Message
-				if event.type == Line::Bot::Event::MessageType::Text
-					line_uid = event['source']['userId']
-					message = event.message['text'].strip
-					weather = User.get_weather(message)
-					if weather
-						user = User.find_or_create_by(line_uid: line_uid)
-						#天気予報用フォーマットに沿ったテキストを返すメソッド。文末の改行を消すと綺麗に表示される。
-						p weather
-						response_message = User.format_weather_response(weather).strip
-					else
-						response_message = "天気情報を取得できませんでした。正しい場所を入力してください。
-											例:Tokyo,Saitama,Kawagoe"
-					end
-					client.reply_message(event['replyToken'], { type: 'text', text: response_message })
-				else
-					response_message = "そのメッセージには対応していません！"
-					client.reply_message(event['replyToken'], { type: 'text', text: response_message })
-				end
-			end
-		end
-		head :ok
-	end
+        events.each do |event|
+            case event
+            when Line::Bot::Event::Message
+                if event.type == Line::Bot::Event::MessageType::Text
+                    line_uid = event['source']['userId']
+                    message = event.message['text'].strip
+                    weather_forecasts = User.get_weather(message) # 複数の天気データを取得
+                    if weather_forecasts
+                        user = User.find_or_create_by(line_uid: line_uid)
+                        puts weather_forecasts
+                        response_message = User.format_weather_responses(weather_forecasts).strip
+                    else
+                        response_message = "天気情報を取得できませんでした。正しい場所を入力してください。
+                                            例:Tokyo,Saitama,Kawagoe"
+                    end
+                    client.reply_message(event['replyToken'], { type: 'text', text: response_message })
+                else
+                    response_message = "そのメッセージには対応していません！"
+                    client.reply_message(event['replyToken'], { type: 'text', text: response_message })
+                end
+            end
+        end
+        head :ok
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,29 +2,36 @@ class User < ApplicationRecord
     validates :line_uid, presence: true
 
     API_KEY = ENV['WEATHER_APIKEY']
-	class << self
-		def get_weather(location)
-			response = HTTParty.get("http://api.openweathermap.org/data/2.5/forecast?q=#{location},jp&APPID=#{API_KEY}&lang=ja&units=metric")
-			if response.success?
-				data = response.parsed_response #HTTPartyのparsed_responseは解析されたJSONを返す
-				{
-					temp_max: (data['list'][1]['main']['temp_max']).round(2),
-					temp_min: (data['list'][1]['main']['temp_min']).round(2),
-					description: data['list'][1]['weather'][0]['description'],
-					dt_txt: data['list'][1]['dt_txt'],
-				}
-			else
-				Rails.logger.error("Weather API request failed: #{response.code} #{response.message}")
-				nil
-			end
-		end
-
-        def format_weather_response(weather_forecast)
-            <<~TEXT
-                日時: #{weather_forecast[:dt_txt]}
-                天気: #{weather_forecast[:description]}
-                気温: #{weather_forecast[:temp_min]}°C ~ #{weather_forecast[:temp_max]}°C
-            TEXT
+    class << self
+        def get_weather(location)
+            response = HTTParty.get("http://api.openweathermap.org/data/2.5/forecast?q=#{location},jp&APPID=#{API_KEY}&lang=ja&units=metric")
+            if response.success?
+                data = response.parsed_response
+                data['list'][1..6].map do |item|
+                    {
+                        temp_max: (item['main']['temp_max']).round(2),
+                        temp_min: (item['main']['temp_min']).round(2),
+                        humidity: (item['main']['humidity']).round(2),
+                        description: item['weather'][0]['description'],
+                        dt_txt: item['dt_txt'],
+                    }
+                end
+            else
+                Rails.logger.error("Weather API request failed: #{response.code} #{response.message}")
+                nil
+            end
         end
-	end
+
+        def format_weather_responses(weather_forecasts)
+            # 複数の天気データをフォーマット
+            weather_forecasts.map do |forecast|
+                <<~TEXT
+                    日時: #{forecast[:dt_txt]}
+                    天気: #{forecast[:description]}
+                    気温: #{forecast[:temp_min]}°C ~ #{forecast[:temp_max]}°C
+                    湿度: #{forecast[:humidity]}%
+                TEXT
+            end.join("\n\n") # 各テキストを区切る改行を追加
+        end
+    end
 end
