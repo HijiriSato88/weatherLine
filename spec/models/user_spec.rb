929ec8984f2bf7f3b10863210431e622716fe5db
HijiriSato88 <satohiji0808@outlook.com>
2025-02-02 17:53:50 +0900

    User.get_wetherメソッドのレスポンステスト

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,7 +16,6 @@ RSpec.describe User, type: :model do
 
   describe '.get_weather' do
     let(:api_url) { "http://api.openweathermap.org/data/2.5/forecast?q=#{location},jp&APPID=#{ENV['WEATHER_APIKEY']}&lang=ja&units=metric" }
-    let(:response_body) { '{}' }
     let(:headers) { { 'Content-Type' => 'application/json' } }
 
     before { WebMock.stub_request(:get, api_url).to_return(status: status, body: response_body, headers: headers) }
@@ -24,21 +23,41 @@ RSpec.describe User, type: :model do
     context 'when response is 200 OK' do
       let(:location) { 'Tokyo' }
       let(:status) { 200 }
+      let(:response_body) do
+        {
+          city: { name: location },
+          list: Array.new(6) do |i|
+            {
+              main: { temp_max: 25.567, temp_min: 18.432, humidity: 75 },
+              weather: [{ description: '晴れ' }],
+              dt_txt: "2025-02-02 12:00:00"
+            }
+          end
+        }.to_json
+      end
 
-      it 'returns a 200 OK response' do
-        response = HTTParty.get(api_url)
-        # puts response.body
-        expect(response.code).to eq(200)
+      it 'returns weather data for the city' do
+        response = User.get_weather(location)
+        expect(response).not_to be_nil
+        expect(response[:city_name]).to eq('Tokyo')
+
+        first_forecast = response[:weather_forecasts].first
+        expect(first_forecast[:temp_max]).to eq(25.57)
+        expect(first_forecast[:temp_min]).to eq(18.43)
+        expect(first_forecast[:humidity]).to eq(75)
+        expect(first_forecast[:description]).to eq('晴れ')
+        expect(first_forecast[:dt_txt]).to eq('2025-02-02 12:00:00')
       end
     end
 
-    context 'when miss send location' do
+    context 'when location is invalid' do
       let(:location) { 'Hello' }
       let(:status) { 404 }
+      let(:response_body) { '{"cod":"404","message":"city not found"}' }
 
-      it 'returns a 404 response' do
-        response = HTTParty.get(api_url)
-        expect(response.code).to eq(404)
+      it 'returns nil' do
+        response = User.get_weather(location)
+        expect(response).to be_nil
       end
     end
   end
