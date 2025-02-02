98b5d667f892a915a53793b2638daa2b39ddcb90
HijiriSato88 <satohiji0808@outlook.com>
2025-02-02 23:17:48 +0900

    メソッド分割

--- app/controllers/line_bot_controller.rb
+++ app/controllers/line_bot_controller.rb
@@ -2,7 +2,6 @@
 
 class LineBotController < ApplicationController
   def client
-    # @clientが未定義の場合のみ,新しいクライアントを作成
     @client ||= Line::Bot::Client.new do |config|
       config.channel_secret = ENV.fetch('LINE_CHANNEL_SECRET', nil)
       config.channel_token = ENV.fetch('LINE_CHANNEL_TOKEN', nil)
@@ -11,32 +10,41 @@ class LineBotController < ApplicationController
 
   def callback
     body = request.body.read
-
     signature = request.env['HTTP_X_LINE_SIGNATURE']
     head 470 unless client.validate_signature(body, signature)
 
     events = client.parse_events_from(body)
 
     events.each do |event|
-      case event
-      when Line::Bot::Event::Message
-        if event.type == Line::Bot::Event::MessageType::Text
-          line_uid = event['source']['userId']
-          client_message = event.message['text'].strip
-          weather_forecasts = User.get_weather(client_message) # クライアントからの地名メッセージを基に、天気予報を取得
-          if weather_forecasts
-            User.find_or_create_by(line_uid: line_uid)
-            response_message = User.format_weather_responses(weather_forecasts).strip
-          else
-            response_message = "天気情報を取得できませんでした。正しい場所を入力してください。
-                                            例:Tokyo,Saitama,Kawagoe"
-          end
-        else
-          response_message = 'そのメッセージには対応していません！'
-        end
-        client.reply_message(event['replyToken'], { type: 'text', text: response_message })
-      end
+      handle_text_message(event) if text_message?(event)
     end
+
     head :ok
+  end
+
+  private
+
+  def text_message?(event)
+    event.is_a?(Line::Bot::Event::Message) && event.type == Line::Bot::Event::MessageType::Text
+  end
+
+  def handle_text_message(event)
+    line_uid = event['source']['userId']
+    client_message = event.message['text'].strip
+
+    response_message = generate_response_message(line_uid, client_message)
+
+    client.reply_message(event['replyToken'], { type: 'text', text: response_message })
+  end
+
+  def generate_response_message(line_uid, client_message)
+    weather_forecasts = User.get_weather(client_message)
+
+    if weather_forecasts
+      User.find_or_create_by(line_uid: line_uid)
+      User.format_weather_responses(weather_forecasts).strip
+    else
+      "天気情報を取得できませんでした。正しい場所を入力してください。\n例:Tokyo,Saitama,Kawagoe"
+    end
   end
 end
